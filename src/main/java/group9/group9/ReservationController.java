26e9c472c1c6a65dee7f95fcfdf088f9f817a504
Mio Waller <38918176+MioWaller@users.noreply.github.com>
2021-11-27 21:48:08 -0600

    Commented out all the unused imports

--- src/main/java/group9/group9/ReservationController.java
+++ src/main/java/group9/group9/ReservationController.java
@@ -1,16 +1,16 @@
 package group9.group9;
 
-import java.util.List;
+//import java.util.List;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.Cookie;
+//import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.*;
