b692bdbec867e11e70ad54249a2dc63c2117f1f5
Mio Waller <38918176+MioWaller@users.noreply.github.com>
2021-12-01 02:26:58 -0600

    Created ReservationEntityTests.java

--- src/test/java/group9/group9/ReservationEntityTests.java
+++ src/test/java/group9/group9/ReservationEntityTests.java
@@ -1,5 +1,37 @@
 package group9.group9;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.TestTemplate;
+
 public class ReservationEntityTests {
+    @Test
+    void gettersAndSettersWork() {
+        ReservationEntity entity = new ReservationEntity();
+
+        assertThat(entity).isNotNull();
+
+        entity.setFullName("group9");
+        entity.setPhoneNumber("0000000000");
+        entity.setEmail("group9@gmail.com");
+        entity.setDate("00/00/0000");
+        entity.setTime("00:00 PM");
+        entity.setNumOfGuests(3);
+        entity.setHoliday(false);
+        entity.setId(10);
+        entity.setUserId(9);
+
+        assertEquals(entity.getFullName(), "group9");
+        assertEquals(entity.getPhoneNumber(), "0000000000");
+        assertEquals(entity.getEmail(), "group9@gmail.com");
+        assertEquals(entity.getDate(), "00/00/0000");
+        assertEquals(entity.getTime(), "00:00 PM");
+        assertEquals(entity.getNumOfGuests(), 3);
+        assertEquals(entity.isHoliday(), false);
+        assertEquals(entity.getId(), 10);
+        assertEquals(entity.getUserId(), 9);
+    }
     
 }
