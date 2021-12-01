dfce6dc3a9c4d6249fb18409b76ad08662911cfc
Mio Waller <38918176+MioWaller@users.noreply.github.com>
2021-12-01 01:43:03 -0600

    Updates

--- src/test/java/group9/group9/UserInfoEntityTests.java
+++ src/test/java/group9/group9/UserInfoEntityTests.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.yaml.snakeyaml.emitter.Emitter;
 
 public class UserInfoEntityTests {
     @Test
